6948e32770dfd98746f6648017e9b0a9a60b749b
liulvjian <634098330@qq.com>
2017-07-26 18:16:02 +0800

    add Edit

--- src/components/author/Edit.tsx
+++ src/components/author/Edit.tsx
@@ -1,9 +1,65 @@
 import * as React from 'react';
+import { Card, Form, Input, DatePicker, Button, message } from 'antd';
+import { RouteComponentProps } from 'react-router-dom';
 
-function Edit() {
-  return (
-    <div>Edit</div>
-  );
+interface Author {
+  name: string;
+  birthday: string;
+  nationality: string;
+}
+interface Props extends RouteComponentProps<string> {
+  form: any;
+  data: Author[];
 }
 
-export default Edit;
+class Edit extends React.Component<Props, {}> {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handelCancel = this.handelCancel.bind(this);
+  }
+
+  handleSubmit() {
+    message.info(this.props.form.getFieldsValue().birthday.format('YYYY-MM-DD'));
+  }
+
+  handelCancel() {
+    this.props.history.goBack();
+  }
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24, offset: 1 },
+        sm: { span: 1 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 5 },
+      },
+    };
+    return (
+      <Card title="作者编辑">
+        <div style={{ padding: 20 }}>
+          <Form layout="vertical" onSubmit={this.handleSubmit}>
+            <Form.Item label="姓名" {...formItemLayout} style={{ fontSize: 20 }}>
+              {getFieldDecorator('name', { rules: [{ required: true }] })(<Input />)}
+            </Form.Item>
+            <Form.Item label="生日" {...formItemLayout} style={{ fontSize: 20 }}>
+              {getFieldDecorator('birthday', { rules: [{ required: true }] })(<DatePicker />)}
+            </Form.Item>
+            <Form.Item label="国籍" {...formItemLayout} style={{ fontSize: 20 }}>
+              {getFieldDecorator('nationality', { rules: [{ required: true }] })(<Input />)}
+            </Form.Item>
+            <Form.Item {...formItemLayout}>
+              <Button type="primary" htmlType="submit" style={{ marginLeft: 130 }}>提交</Button>
+              <Button onClick={this.handelCancel} style={{ marginLeft: 10 }}>取消</Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </Card>
+    );
+  }
+}
+
+export default Form.create()(Edit);
