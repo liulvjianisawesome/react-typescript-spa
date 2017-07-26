011d8399d83aa66d4a61c2d6d100fba0acdec731
liulvjian <634098330@qq.com>
2017-07-26 11:37:33 +0800

    add typechecking to TableList

--- src/components/author/TableList.tsx
+++ src/components/author/TableList.tsx
@@ -2,17 +2,25 @@ import * as React from 'react';
 // import { Link } from 'react-router-dom';
 import { Table } from 'antd';
 
-function TableList() {
+interface DataType {
+  key: number;
+  id: number;
+  name: string;
+  nationality: string;
+  birthday: string;
+}
+
+interface TableListProps {
+  data: DataType[];
+}
+
+function TableList(props: TableListProps) {
   // const { data, history, fetchData } = props;
-  const dataSource = [
-    { key: 1, id: 1, name: '小排骨', nationality: '中国', birthday: '2012-01-21'},
-    { key: 2, id: 2, name: '大排骨', nationality: '好色之国', birthday: '2202-01-21'},
-    { key: 3, id: 3, name: '毛哥', nationality: '蛮牛国', birthday: '1912-01-21'}
-  ];
+  const { data } = props;
 
   return (
     <Table
-      dataSource={dataSource}
+      dataSource={data}
       columns={[
         { title: 'id', dataIndex: 'id', key: 'id' },
         { title: '姓名', dataIndex: 'name', key: 'name' },
